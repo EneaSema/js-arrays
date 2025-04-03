3fbd9c0b9bc4e6f34e45ed3c71d29a83ae4755a8
EneaSema <sema.enea1993@gmail.com>
2025-04-03 16:41:09 +0200

    third-solution-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -7,21 +7,29 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = "";
+const longNames = null;
+
+console.log(longNames);
+
 // controllo partendo da 0 tutti gli insegnanti nell'array
 for (let i = 0; i < teachers.length; i++) {
   //console.log(teachers);
-  if (teachers[i].indexOf() >= 5) {
-    longNames = longNames + teachers[i];
-    console.log(longNames);
+  //per l'insegnante che ho, controllo il numero di caratteri e se uguale o superiore a 5, inserisco nel nuovo array
+  //creo variabile da appoggio
+  let newArray = "";
+
+  if (teachers[i].length >= 5) {
+    newArray += teachers[i];
+    console.log(newArray);
   }
 }
-console.log(teachers);
+//longNames[newArray];
+// console.log(longNames);
 
-//if (teachers[i] >= 5) {
-//longNames = longNames + teachers[i];
-//console.log(longNames);
 // 3. Rimuovi 'Ed' dall'array teachers
+console.log(teachers);
+teachers.splice(5, 1);
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
